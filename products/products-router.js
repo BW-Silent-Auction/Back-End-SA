e0157748fced7e9cf3af0c8b592c568f49cc55bf
Alex <agordon1123@gmail.com>
2019-09-22 16:10:50 -0400

    created bid GET and POST endpoints

--- products/products-router.js
+++ products/products-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Products = require('./products-model');
+const Bids = require('./bids-model');
 
 const router = express.Router();
 
@@ -52,6 +53,34 @@ router.put('/:id', (req, res) => {
     } else {
         Products.update(id, req.body)
             .then(success => res.status(200).json(success))
+            .catch(err => res.status(500).json(err));
+    };
+});
+
+router.get('/:id/bids', (req, res) => {
+    const { id } = req.params;
+
+    Bids.find(id)
+        .then(success => {
+            if (success && success.length) {
+                res.status(200).json(success)
+            } else {
+                res.status(404).json({ error: 'The product with the provided ID does not exist' });
+            };
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+router.post('/:id/bids', (req, res) => {
+    req.body.product_id = req.params.id;
+    
+    const { buyer_id, bid_amount } = req.body;
+
+    if (!buyer_id || !bid_amount) {
+        res.status(400).json({ error: 'Please include a valid buyer_id and bid_amount with the request' });
+    } else {
+        Bids.add(req.body)
+            .then(success => res.status(201).json(success))
             .catch(err => res.status(500).json(err));
     };
 });
