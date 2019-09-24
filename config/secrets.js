25c980546774c86adc9a7229b8e8c06fb0c031a6
Alex <agordon1123@gmail.com>
2019-09-24 19:25:45 -0400

    seeing what I can delete

--- config/secrets.js
+++ config/secrets.js
@@ -3,7 +3,7 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET || 'Silence is golden',
     environment: process.env.NODE_ENV,
     cloudinaryName: process.env.CLOUDINARY_NAME || 'hk7xbpwvb',
-    cloudinaryApiKey: process.env.API_KEY || '827372394628198',
-    cloudinarySecret: process.env.API_Secret || 'XPN7BGn1xisVQMES6e8jQT-Ogl0',
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL || 'cloudinary://827372394628198:XPN7BGn1xisVQMES6e8jQT-Ogl0@hk7xbpwvb'
+    // cloudinaryApiKey: process.env.API_KEY || '827372394628198',
+    // cloudinarySecret: process.env.API_Secret || 'XPN7BGn1xisVQMES6e8jQT-Ogl0',
+    // CLOUDINARY_URL: process.env.CLOUDINARY_URL || 'cloudinary://827372394628198:XPN7BGn1xisVQMES6e8jQT-Ogl0@hk7xbpwvb'
 };
