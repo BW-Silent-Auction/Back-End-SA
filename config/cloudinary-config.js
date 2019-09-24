bdb220e3bc7965286c5e746b59ad9485238ca0be
Alex <agordon1123@gmail.com>
2019-09-24 18:31:37 -0400

    removed v2

--- config/cloudinary-config.js
+++ config/cloudinary-config.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 console.log(cloudinaryStorage);
 const multer = require('multer');
